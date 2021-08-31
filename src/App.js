b08cacf82f6889e8c9a809402c39d3383be7c8d0
Vincent-Juma <vincentjuma64@gmail.com>
2021-08-31 04:05:13 +0300

    Consumption of API

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import React, { useEffect} from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
-  return (
+  
+    useEffect (async() => {
+      const jokes = await axios.get("https://api.chucknorris.io/jokes/categories");
+      console.log(jokes.data);
+    }, [])
+
+    return (
     <div>
       <h1>Welcome for the chuck demo</h1>
     </div>
